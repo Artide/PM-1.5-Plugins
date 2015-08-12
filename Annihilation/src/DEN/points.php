41c14ee1eb99146975273e8e0097da83423a4940
Eduardo Tellez <ippalin@gmail.com>
2015-08-11 15:08:34 -1000

    Update points.php

--- Annihilation/src/DEN/points.php
+++ Annihilation/src/DEN/points.php
@@ -13,7 +13,28 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
 
 class points{
+
+    public $points = 0;
+
+    public function __construct(points $plugin){
+
+        $this->plug = $plugin;
+
+    }
+
+    public function onDeath(PlayerDeathEvent $e){
+
+        $victim = $e->getEntitiy();
+        if($victim instanceof Player){
+            $points = new Config($this->getDataFolder() . "points/" . strtolower($victim->getName() . ".yml", Config::YAML));
+
+            //TODO
+            
+        }
+
+    }
 
 }
