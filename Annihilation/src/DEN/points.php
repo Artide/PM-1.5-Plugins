899a2d514fef8e99a508225942f1e2965cdb59af
Eduardo Tellez <ippalin@gmail.com>
2015-08-20 15:33:31 -1000

    Update points.php

--- Annihilation/src/DEN/points.php
+++ Annihilation/src/DEN/points.php
@@ -13,27 +13,67 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\player\PlayerDeathEvent;
-use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
 
-class points{
+class points
+{
 
-    public $points = 0;
-
-    public function __construct(points $plugin){
+    public function __construct(points $plugin)
+    {
 
         $this->plug = $plugin;
 
     }
 
-    public function onDeath(PlayerDeathEvent $e){
+    public function onDeath(EntityDamageEvent $e, $msg, $pts){
 
         $victim = $e->getEntitiy();
-        if($victim instanceof Player){
-            $points = new Config($this->getDataFolder() . "points/" . strtolower($victim->getName() . ".yml", Config::YAML));
+        if ($victim instanceof Player) {
 
-            //TODO
-            
+            $victim->sendMessage($this->getConfig()->get("PlayerDeathMessage");
+
+            }
+
+        $damager = $victim->getLastDamageCause()->getCause();
+
+        if ($damager = 1) {
+
+            $attacker = $victim->getLastDamageCause()->getCause();
+
+            if ($attacker instanceof Player) {
+
+                $apoints = new Config($this->getDataFolder() . "points/" . strtolower($victim->getName()) . ".yml", Config::YAML));
+                if ($apoints->exists("Points")) {
+
+                    $apoints->set($apoints->get("Points") + $this->getConfig()->get("GainedPoints"));
+                    $apoints->save();
+                    $attacker->sendMessage(Main::pfx . $this->getConfig()->get("GainedPointsMessage"));
+                    return true;
+
+                } else {
+
+                    $apoints->set(array($apoints->get("Points") => $this->getConfig()->get("GainedPoints")));
+                    $apoints->save();
+                    $attacker->sendMessage(Main::pfx . $this->getConfig()->get("GainedPointsMessage"));
+                    return true;
+
+                }
+
+            }
+
         }
+
+        $msg = $this->getConfig()->getAll();
+        $msg = str_replace("{VICTIM}",$victim->getName(),$msg);
+        $msg = str_replace("{ATTACKER}", $attacker->getName(), $msg);
+
+        return $msg;
+
+        $pts = $this->getConfig()->getAll();
+        $pts = str_replace("{POINTS}", $apoints->get("Points"),$pts);
+        $pts = str_replace("{GAINED}", $this->getConfig()->get("GainedPoints"),$pts);
+
+        return $pts;
 
     }
 
