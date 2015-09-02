7101e254ba4012d0edebcc61092ce962c4d18fe0
Eduardo Tellez <ippalin@gmail.com>
2015-09-01 14:03:57 -1000

    Create Nexuxs.php

---
+++ Annihilation/src/DEN/Nexuxs.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace DEN;
+
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\level\Level;
+use pocketmine\math\Vector3;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\BlockBreakEvent as onNexusBreak;
+
+class Nexus{
+    
+    
+}
