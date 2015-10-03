4375b387a1ab4d3c3f74fbc5ac62341b5f428410
Eduardo Tellez <ippalin@gmail.com>
2015-10-03 14:46:30 -0700

    removed unnecessary spaces

--- RandTeleport/src/RandTeleport/Main.php
+++ RandTeleport/src/RandTeleport/Main.php
@@ -14,25 +14,21 @@ use pocketmine\command\CommandExecutor;
 
 class Main extends PluginBase{
 
-    public function onEnable(){
-
+    public function onEnable()
+    {
         $log = $this->getLogger();
-
         $log->info("Plugin successfully loaded!");
-
         $this->saveDefaultConfig();
         $this->reloadConfig();
-
     }
 
     /**
      * @param $p
      * @return mixed
      */
-    public function rand_tp($p){
-
+    public function rand_tp($p)
+    {
         return $p->teleport($p->getLevel()->getSafeSpawn(new Vector3(mt_rand(0,1000), mt_rand(0,127), mt_rand(0,1000))));
-
     }
 
     /**
@@ -43,43 +39,26 @@ class Main extends PluginBase{
      * @return bool
      */
     public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
-
         switch(strtolower($cmd->getName())) {
-
             case 'rand':
-
                 foreach($this->getConfig()->get("Worlds") as $world) {
-
                     if ($sender->hasPermission("randtp.rand")) {
-
                         if ($sender instanceof Player && $sender->getLevel()->getName() !== $world) {
-
                             $this->rand_tp($sender);
-
                             $sender->sendMessage(TXT::AQUA . "Teleported to X: " . TXT::YELLOW . $sender->getX()
                                 . TXT::AQUA . ", Y: " . TXT::YELLOW . $sender->getY() . TXT::AQUA . " Z: " . TXT::YELLOW . $sender->getX());
-
                         } elseif(!$sender instanceof Player) {
-
                             $sender->sendMessage(TXT::RED . "Please run this command in game.");
 
                             return true;
-
                         }elseif($sender->getLevel()->getName() === $world){
-
                             $sender->sendMessage(TXT::RED . "You are not allowed to use that command in this world.");
 
                             return true;
-
                         }
-
                     }
-
                 }
              break;
-
         }
-
     }
-
 }
