aff60abd07b011f47f3228193a5d49a1441ac39b
Eduardo Tellez <ippalin@gmail.com>
2015-10-04 20:23:24 -0700

    Added small API and code cleanup

--- RandTeleport/src/RandTeleport/Main.php
+++ RandTeleport/src/RandTeleport/Main.php
@@ -1,7 +1,5 @@
 <?php
-
-namespace RandTeleport;
-
+namespace RandTP;
 use pocketmine\Level;
 use pocketmine\Player;
 use pocketmine\Server;
@@ -11,8 +9,13 @@ use pocketmine\utils\TextFormat as TXT;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\CommandSender;
 use pocketmine\command\CommandExecutor;
+class RandTP extends PluginBase
+{
 
-class Main extends PluginBase{
+    const SMALL_TP = 0;
+    const MEDIUM_TP = 1;
+    const RANDOM_TP = 3;
+    const FAR_TP = 4;
 
     public function onEnable()
     {
@@ -24,11 +27,24 @@ class Main extends PluginBase{
 
     /**
      * @param $p
+     * @param $type
      * @return mixed
      */
-    public function rand_tp($p)
-    {
-        return $p->teleport($p->getLevel()->getSafeSpawn(new Vector3(mt_rand(0,1000), mt_rand(0,127), mt_rand(0,1000))));
+    public function toRand($p, $type){
+        switch($type){
+            case self::SMALL_TP:
+                return $p->teleport($p->getLevel()->getSafeSpawn(new Vector3(mt_rand(0, 100), mt_rand(0, 127), mt_rand(0, 100))));
+            break;
+            case self::MEDIUM_TP:
+                return $p->teleport($p->getLevel()->getSafeSpawn(new Vector3(mt_rand(0, 500), mt_rand(0, 127), mt_rand(0, 500))));
+            break;
+            case self::RANDOM_TP:
+                return $p->teleport($p->getLevel()->getSafeSpawn(new Vector3(mt_rand(0, 1000), mt_rand(0, 127), mt_rand(0, 1000))));
+            break;
+            case self::FAR_TP:
+                return $p->teleport($p->getLevel()->getSafeSpawn(new Vector3(mt_rand(0, 5000), mt_rand(0, 127), mt_rand(0, 5000))));
+            break;
+        }
     }
 
     /**
@@ -44,21 +60,19 @@ class Main extends PluginBase{
                 foreach($this->getConfig()->get("Worlds") as $world) {
                     if ($sender->hasPermission("randtp.rand")) {
                         if ($sender instanceof Player && $sender->getLevel()->getName() !== $world) {
-                            $this->rand_tp($sender);
+                            self::toRand($sender, self::RANDOM_TP);
                             $sender->sendMessage(TXT::AQUA . "Teleported to X: " . TXT::YELLOW . $sender->getX()
                                 . TXT::AQUA . ", Y: " . TXT::YELLOW . $sender->getY() . TXT::AQUA . " Z: " . TXT::YELLOW . $sender->getX());
                         } elseif(!$sender instanceof Player) {
                             $sender->sendMessage(TXT::RED . "Please run this command in game.");
-
                             return true;
                         }elseif($sender->getLevel()->getName() === $world){
                             $sender->sendMessage(TXT::RED . "You are not allowed to use that command in this world.");
-
                             return true;
                         }
                     }
                 }
-             break;
+                break;
         }
     }
 }
