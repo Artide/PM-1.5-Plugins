625726288c8a5cad7532d1208bc407d5f011ac7c
Eduardo Tellez <ippalin@gmail.com>
2015-10-06 16:37:09 -0700

    Update WU.php

--- WhatsUp/src/WU/WU.php
+++ WhatsUp/src/WU/WU.php
@@ -8,6 +8,7 @@ use pocketmine\command\Command;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat as TXT;
+use pocketmine\command\ConsoleCommandSender;
 
 class WU extends PluginBase{
 
@@ -19,33 +20,41 @@ class WU extends PluginBase{
         $this->reloadConfig();
     }
 
+    /**
+     * @param $player
+     * @param $message
+     * @return mixed
+     */
     public function WhatsUp($player, $message)
     {
         $format = $this->getConfig()->get("Format");
         return $player->setNameTag(str_replace(["@player", "@whats_up"], [$player->getName(), $message], $format));
     }
 
+    /**
+     * @param CommandSender $p
+     * @param Command $c
+     * @param $label
+     * @param array $args
+     */
     public function onCommand(CommandSender $p, Command $c, $label, array $args)
     {
         switch(strtolower($c->getName())){
             //case 'whatsup':
             case 'wu':
                 if($p->hasPermission("wu.wu")){
-                    foreach($args as $status){
-                        self::WhatsUp($p, $status);
-                        $p->sendMessage(TXT::GREEN . "Status set to '" . $status . "'.");
-                        return;
+                    if($p instanceof Player) {
+                        self::WhatsUp($p, implode(" ", $args));
+                        $p->sendMessage(TXT::GREEN . "Status set to '" . implode(" ", $args) . "'.");
+                    }else{
+                        $p->sendMessage(TXT::RED . "Please run this command in-game.");
                     }
-                    if(count($args) < 1){
+                    if(empty($args) && count($args) === 0){
                         $p->sendMessage(TXT::RED . "Failed to set status. Usage: /wu <status>");
                     }else{
                         return;
                     }
-                    if(!$p instanceof Player){
-                        $p->sendMessage(TXT::RED . "Please run this command in-game.");;
-                    }else{
-                        return;
-                    }
+                    return;
                 }
             break;
         }
