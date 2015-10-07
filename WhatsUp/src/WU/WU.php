0309a1a3ba63ea1fd99f4ff6d4d7ba5c62f243a6
Eduardo Tellez <ippalin@gmail.com>
2015-10-07 13:53:20 -0700

    Update WU.php

--- WhatsUp/src/WU/WU.php
+++ WhatsUp/src/WU/WU.php
@@ -4,13 +4,15 @@ namespace WU;
 
 use pocketmine\Player;
 use pocketmine\Server;
+use pocketmine\utils\Config;
+use pocketmine\event\Listener;
 use pocketmine\command\Command;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat as TXT;
-use pocketmine\command\ConsoleCommandSender;
+use pocketmine\event\player\PlayerJoinEvent;
 
-class WU extends PluginBase{
+class WU extends PluginBase implements Listener{
 
     public function onEnable()
     {
@@ -18,6 +20,9 @@ class WU extends PluginBase{
         $log->info(TXT::GREEN . "Plugin enabled!");
         $this->saveDefaultConfig();
         $this->reloadConfig();
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        @mkdir($this->getDataFolder());
+        @mkdir($this->getDataFolder() . "WhatsUp");
     }
 
     /**
@@ -32,6 +37,36 @@ class WU extends PluginBase{
     }
 
     /**
+     * @param $player
+     * @param $message
+     */
+    public function saveStatus($player, $message)
+    {
+        @mkdir($this->getDataFolder());
+        file_put_contents($this->getDataFolder() . "WhatsUp/" . $player->getName() . ".yml", yaml_emit(["Status" => implode(" ", $message)]));
+        if(!empty($message)) {
+            $player->sendMessage(TXT::GREEN . "Successfully saved status as '" . implode(" ", $message) . "'.");
+        }else{
+            return;
+        }
+        return;
+    }
+
+    /**
+     * @param $player
+     */
+    public function deleteStatus($player)
+    {
+        if(file_exists($this->getDataFolder() . "WhatsUp/" . $player->getName() . ".yml")){
+            unlink($this->getDataFolder() . "WhatsUp/" . $player->getName() . ".yml");
+            $player->sendMessage(TXT::GREEN . "Successfully deleted status.");
+        }else{
+            $player->sendMessage(TXT::RED . "No status found for you. :(");
+        }
+        return;
+    }
+
+    /**
      * @param CommandSender $p
      * @param Command $c
      * @param $label
@@ -39,24 +74,64 @@ class WU extends PluginBase{
      */
     public function onCommand(CommandSender $p, Command $c, $label, array $args)
     {
-        switch(strtolower($c->getName())){
+        switch(strtolower($c->getName())) {
             //case 'whatsup':
             case 'wu':
-                if($p->hasPermission("wu.wu")){
-                    if($p instanceof Player) {
+                if ($p->hasPermission("wu.wu")) {
+                    if ($p instanceof Player) {
                         self::WhatsUp($p, implode(" ", $args));
                         $p->sendMessage(TXT::GREEN . "Status set to '" . implode(" ", $args) . "'.");
-                    }else{
+                    } else {
                         $p->sendMessage(TXT::RED . "Please run this command in-game.");
                     }
-                    if(empty($args) && count($args) === 0){
+                    if (empty($args) || count($args) === 0) {
                         $p->sendMessage(TXT::RED . "Failed to set status. Usage: /wu <status>");
-                    }else{
+                    } else {
                         return;
                     }
-                    return;
+                }
+                break;
+            //case 'savestatus':
+            case 'ss':
+                if ($p->hasPermission("wu.ss")) {
+                    if ($p instanceof Player) {
+                        self::WhatsUp($p, implode(" ", $args));
+                        self::saveStatus($p, $args);
+                    } else {
+                        $p->sendMessage(TXT::RED . "Please run this command in-game.");
+                    }
+                    if (empty($args) || count($args) === 0) {
+                        $p->sendMessage(TXT::RED . "Failed to set status. Usage: /ss <status>");
+                    } else {
+                        return;
+                    }
                 }
             break;
+            //case 'delstatus':
+            case 'ds':
+                if ($p->hasPermission("wu.ds")) {
+                    if ($p instanceof Player) {
+                        self::deleteStatus($p);
+                    } else {
+                        $p->sendMessage(TXT::RED . "Please run this command in-game.");
+                    }
+                }
+            break;
+        }
+    }
+
+    /**
+     * @param PlayerJoinEvent $e
+     */
+    public function onPlayerJoin(PlayerJoinEvent $e)
+    {
+        $p = $e->getPlayer();
+        if(file_exists($this->getDataFolder() . "WhatsUp/" . $p->getName() . ".yml")){
+            $status = new Config($this->getDataFolder() . "WhatsUp/" . $p->getName() . ".yml", Config::YAML);
+            $p->setNameTag($status->get("Status"));
+            $p->sendMessage(TXT::GREEN . "Status set to '" . $status->get("Status") . "'.");
+        }else{
+            return;
         }
     }
 }
